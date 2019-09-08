def812be49effaf7196100f83abe89b9f89c9562
小棹雄司 <yujikosao@me.com>
2019-09-08 12:55:51 +0900

    add アメブロ対応

---
+++ ameblo.py
@@ -0,0 +1,13 @@
+class Rsser:
+    def __init__(self,url):
+        self.url = url
+    def getRss(self):
+        rssurl = self.url.replace("https://ameblo.jp/","").strip("/")
+        rssurl = "http://rssblog.ameba.jp/" + rssurl + "/rss20.xml"
+        return rssurl
+url = input('rssを取得したいブログのurlを入力してください\n')
+
+rss = Rsser(url)
+
+print('rssは以下です')
+print(rss.getRss())
