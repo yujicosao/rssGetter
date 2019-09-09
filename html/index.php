e76b35ab31d55cb285c49ebdc77570c7411229a5
小棹雄司 <yujikosao@me.com>
2019-09-09 17:36:16 +0900

    update:  bootstrapCDN追加

--- html/index.php
+++ html/index.php
@@ -7,18 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>RSS変換アプリ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="./scripts.js"></script>
 </head>
 <body>
-    <form action="./index.php" method="post">
-        <p>RSSに変換したいブログページを入力してください</p>
-        <input type="text" name="rss">
-        <input type="submit" value="変換する">
-    </form>
-
-    <input id="copyTarget" type="text" value="<?php echo $rss_url ?>" readonly>
-    <button onclick="copyToClipboard()">コピー</button>
-
+    <div class="container">
+        <form action="./index.php" method="post">
+            <h1>RSSに変換したいブログページを入力してください</h1>
+            <div class="form-group form-inline">
+                <input type="text" name="rss" class="form-control">
+                <input type="submit" value="変換する"  class="btn btn-primary">
+            </div>
+        </form>
+        <div class="form-group form-inline">
+            <input id="copyTarget" type="text" value="<?php echo $rss_url ?>" readonly  class="form-control">
+            <button onclick="copyToClipboard()" class="btn btn-primary">コピー</button>
+        </div>
+    </div>
 
     <?php
 // if($_SERVER["REQUEST_METHOD"] != "POST"){
